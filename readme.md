5e1cd9c796c45b23299d95dc47d31eaf03cfc2b7
aknjoroge <alexkaranja629@gmail.com>
2024-11-13 20:15:18 +0300

    added single page mark up

--- readme.md
+++ readme.md
@@ -14,6 +14,6 @@ Created using [WP Bootstrap Starter theme](https://github.com/aknjoroge/wp-boots
 
 ## Feature stories
 
-- ### Dynamic menu
+- ### Project Setup using Bootstrap 4
 
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to ma
+Instead of using the template code the Instructor provided, I decided to create the mark up and stylying on my own using Bootstarp 4 & fontawesome
--- themes/university/page.php
+++ themes/university/page.php
@@ -15,34 +15,63 @@
 get_header(); ?>
 
 <div class="page-banner">
-    <div class="page-banner-image"
-        style="background-image: url(<?php echo get_theme_file_uri( '/assets/images/ocean.jpg' ) ?>)">
+  <div class="page-banner-image"
+    style="background-image: url(<?php echo get_theme_file_uri( '/assets/images/ocean.jpg' ) ?>)">
+  </div>
+
+  <div
+    class=" d-flex  justify-content-end flex-column page-banner-content inner-page-banner-content text-center text-white py-5">
+    <div class="text-left container">
+
+      <h1>Our History!</h1>
+      <h3>Learn how the school of your dreams got started.</h3>
     </div>
-
-    <div
-        class=" d-flex  justify-content-end flex-column page-banner-content inner-page-banner-content text-center text-white py-5">
-		<div class="text-left container">
-
-			<h1>Our History!</h1>
-			<h3>Learn how the school of your dreams got started.</h3>
-		</div>
-		</div>
+  </div>
 
 
 </div>
 
-<div>
+<div class="container py-5">
+  <div class="row">
+    <div class="col-md-10">
 
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="#">Home</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Library</li>
+        </ol>
+      </nav>
 
-</div>
-<div class="page-links">
+
+      <div>
+
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid
+        possimus,
+        facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta.
+        Lorem
+        ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit
+        commodi ipsa
+        vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.
+
+      </div>
+
+    </div>
+    <div class="col-md-2">
+      <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
           <li class="current_page_item"><a href="#">Our History</a></li>
           <li><a href="#">Our Goals</a></li>
         </ul>
       </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
 
 <?php
 get_sidebar();
