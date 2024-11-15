c4f7d1477a09063016224016e993ee971848251d
aknjoroge <alexkaranja629@gmail.com>
2024-11-15 06:40:47 +0300

    added permalinks to the child and parent pages

--- themes/university/page.php
+++ themes/university/page.php
@@ -23,7 +23,7 @@ get_header(); ?>
     class=" d-flex  justify-content-end flex-column page-banner-content inner-page-banner-content text-center text-white py-5">
     <div class="text-left container">
 
-      <h1>Our History!</h1>
+      <h1><?php the_title(); ?></h1>
       <h3>Learn how the school of your dreams got started.</h3>
     </div>
   </div>
@@ -35,14 +35,33 @@ get_header(); ?>
   <div class="row">
     <div class="col-md-9">
 
-      <nav style="width: fit-content;" aria-label="breadcrumb">
+      <?php
+
+      $parentId = wp_get_post_parent_id(get_the_ID());
+       
+
+      if($parentId){
+
+        ?>
+
+<nav style="width: fit-content;" aria-label="breadcrumb">
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="#">Home</a></li>
-          <li class="breadcrumb-item active" aria-current="page">Library</li>
+          <li class="breadcrumb-item"><a href="<?php echo get_the_permalink($parentId ); ?>">
+            <?php echo get_the_title($parentId); ?>
+          </a></li>
+          <li class="breadcrumb-item active" aria-current="page">
+            <?php the_title(); ?>
+          </li>
         </ol>
       </nav>
 
+        <?php
 
+        
+
+      }; 
+      ?>
+ 
       <div>
 
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid
@@ -56,21 +75,38 @@ get_header(); ?>
       </div>
 
     </div>
+
     <div class="col-md-3">
       <div class="parent-links">
-        <h2 class="parent"><a href="#">About Us</a></h2>
+        <h2 class="parent"><a href="<?php echo get_the_permalink($parentId  ); ?>">
+          <?php 
+          echo get_the_title( $parentId )
+          ?>
+          </a></h2>
         <ul>
-          <li class="active"><a href=" #">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
+          <?php 
+
+          if ($parentId) {
+            $child_of = $parentId;
+            
+          }else{
+            $child_of = get_the_ID();
+          }
+
+          $child_page_options = array(
+            'title_li' => NULL,
+            'child_of' => $child_of,
+
+          );
+        
+          wp_list_pages($child_page_options);
+          ?>
+           
         </ul>
       </div>
     </div>
   </div>
 </div>
-
-
-
-
 
 
 <?php
