eba8dc12782f8ee15f2a5037cd8db101b8487f76
aknjoroge <alexkaranja629@gmail.com>
2024-12-16 13:36:40 +0300

    added custom validator for plugin settings

--- plugins/word-count-plugin/word-count-plugin.php
+++ plugins/word-count-plugin/word-count-plugin.php
@@ -34,18 +34,19 @@ class WordCountPlugin{
 
    function register_word_count_settings(){
     // Create settings section 
-    add_settings_section( 'wcp_section_1', '<hr/>', array($this, 'section_1_content'), $word_count_plugin_slug );
+    add_settings_section( 'wcp_section_1', '<hr/>', array($this, 'section_1_content'), $word_count_plugin_slug, array('sectiontitle'=> 'Primary settings') );
+    add_settings_section( 'wcp_section_2', '<hr/>', array($this, 'section_1_content'), $word_count_plugin_slug, array('sectiontitle'=> 'Secondary settings') );
 
     // Add fields to the section
     add_settings_field( 'wcp_location', 'Display Location', array($this,'wcp_location_handler'), $word_count_plugin_slug, 'wcp_section_1');
     add_settings_field( 'wcp_headline', 'Headline Text', array($this,'wcp_headline_handler'), $word_count_plugin_slug, 'wcp_section_1');
-    add_settings_field( 'wcp_word_count', 'Word Count', array($this,'wcp_checkbox_handler'), $word_count_plugin_slug, 'wcp_section_1', array('fieldName'=> 'wcp_word_count'));
-    add_settings_field( 'wcp_character_count', 'Character Count', array($this,'wcp_checkbox_handler'), $word_count_plugin_slug, 'wcp_section_1', array('fieldName'=> 'wcp_character_count'));
-    add_settings_field( 'wcp_read_count', 'Read Time', array($this,'wcp_checkbox_handler'), $word_count_plugin_slug, 'wcp_section_1',array('fieldName'=> 'wcp_read_count'));
+    add_settings_field( 'wcp_word_count', 'Word Count', array($this,'wcp_checkbox_handler'), $word_count_plugin_slug, 'wcp_section_2', array('fieldName'=> 'wcp_word_count'));
+    add_settings_field( 'wcp_character_count', 'Character Count', array($this,'wcp_checkbox_handler'), $word_count_plugin_slug, 'wcp_section_2', array('fieldName'=> 'wcp_character_count'));
+    add_settings_field( 'wcp_read_count', 'Read Time', array($this,'wcp_checkbox_handler'), $word_count_plugin_slug, 'wcp_section_2',array('fieldName'=> 'wcp_read_count'));
 
     // Register fields to settings
     register_setting(  'wordcountplugin', 'wcp_location', array(
-        'sanitize_callback' => 'sanitize_text_field',
+        'sanitize_callback' => array($this,'custom_sanitize_function'),
         'default' => '0'
     ) );
 
@@ -67,12 +68,11 @@ class WordCountPlugin{
         'default' => '0'
     ) );
 
-
    }
 
-   function section_1_content(){
+   function section_1_content($args){
     ?>
-    <h4 style="margin:0">Primary settings</h4>
+    <h4 style="margin:0"> <?php echo $args['sectiontitle']; ?></h4>
     <?php 
     }
 
@@ -92,11 +92,22 @@ class WordCountPlugin{
         }
 
     function wcp_checkbox_handler($args){
-
         ?>
         <input type="checkbox" name="<?php echo $args['fieldName']; ?>" <?php checked( get_option($args['fieldName']), 'on' ); ?>  >
         <?php 
+    }
+
+    function custom_sanitize_function($input){
+
+       
+
+        if($input !='1' && $input !='0'){
+            add_settings_error( 'wcp_location','wcp_location_error', 'Invalid display location value' );
+            return  get_option('wcp_location');
         }
+
+        return $input;
+    }
 
     
 
